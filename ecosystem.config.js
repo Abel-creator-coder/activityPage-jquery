1a7bd81f59b5ac505681fa37357a1f62319040d6
lizhanqiang@kanzhun.com <lizhanqiang@kanzhun.com>
2017-08-01 16:51:30 +0800

    lzq

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,7 +30,7 @@ module.exports = {
             user: 'root',
             host: '113.209.11.221',
             ref: 'origin/master',
-            repo: 'git@github.com:lzq2016/test.git',
+            repo: 'https://github.com/lzq2016/test.git',
             path: '/var/local/server/www',
             'post-deploy': 'pm2 reload ecosystem.config.js --env production'
         }
